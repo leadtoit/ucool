8d72f62b7ad4a52bc02ea745e8e28061ffe7418a
czy88840616 <czy88840616@gmail.com>
2011-04-06 00:03:23 +0800

    add a new type to resolve encoding problem

---
+++ src/main/java/biz/url/UrlReader.java
@@ -0,0 +1,50 @@
+package biz.url;
+
+import common.ConfigCenter;
+
+import javax.servlet.ServletOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author <a href="mailto:czy88840616@gmail.com">czy</a>
+ * @since 11-4-5 下午10:31
+ */
+public class UrlReader {
+    /**
+     * 字节读取方法，无视编码，让浏览器自己判断
+     * 
+     * @param outputStream
+     * @param inputStream
+     * @param fileUrl
+     * @param skipCommet
+     * @return
+     * @throws IOException
+     */
+    public boolean pushStream(ServletOutputStream outputStream, InputStream inputStream, String fileUrl, boolean skipCommet) throws IOException {
+        int n;
+        byte[] firstline = new byte[50];
+        if((n = inputStream.read(firstline)) >= 0) {
+            String first = new String(firstline, 0, n);
+            if(first.equals("/*not found*/")) {
+                inputStream.close();
+                outputStream.close();
+                return false;
+            } else {
+                if (!skipCommet) {
+                    outputStream.println("/*ucool filePath=" + fileUrl + "*/");
+                }
+                outputStream.write(firstline, 0, n);
+            }
+        }
+
+        byte[] buf = new byte[1024 * 64];
+        while ((n = inputStream.read(buf)) >= 0) {
+            outputStream.write(buf, 0, n);
+        }
+        inputStream.close();
+        outputStream.flush();
+        outputStream.close();
+        return true;
+    }
+}
--- src/main/java/web/handler/impl/OtherHandler.java
+++ src/main/java/web/handler/impl/OtherHandler.java
@@ -1,6 +1,7 @@
 package web.handler.impl;
 
 import biz.file.FileEditor;
+import biz.url.UrlReader;
 import common.ConfigCenter;
 import common.tools.UrlTools;
 import web.handler.Handler;
@@ -20,15 +21,11 @@ import java.net.URL;
  */
 public class OtherHandler implements Handler {
 
-    private FileEditor fileEditor;
-
     private ConfigCenter configCenter;
 
     private UrlTools urlTools;
 
-    public void setFileEditor(FileEditor fileEditor) {
-        this.fileEditor = fileEditor;
-    }
+    private UrlReader urlReader;
 
     public void setConfigCenter(ConfigCenter configCenter) {
         this.configCenter = configCenter;
@@ -38,36 +35,30 @@ public class OtherHandler implements Handler {
         this.urlTools = urlTools;
     }
 
+    public void setUrlReader(UrlReader urlReader) {
+        this.urlReader = urlReader;
+    }
+
     @Override
     public void doHandler(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         if (request.getRequestURI().indexOf(".swf") != -1) {
             String fullUrl = (String) request.getAttribute("fullUrl");
             fullUrl = urlTools.urlFilter(fullUrl, true, null);
-            //哥对flash没办法，无论怎么取都无法正确展现，只好302
-            //            response.sendRedirect(fullUrl);
-            response.setCharacterEncoding("gbk");
+
             response.setContentType("application/x-shockwave-flash");
-            PrintWriter out = response.getWriter();
             try {
                 URL url = new URL(fullUrl);
-                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "gbk"));
-                fileEditor.pushStream(out, in, null, true);
+                urlReader.pushStream(response.getOutputStream(), url.openStream(), null, true);
             } catch (Exception e) {
             }
-            out.flush();
         } else if (request.getRequestURI().indexOf(".xml") != -1) {
             response.setContentType("text/xml");
-
-            PrintWriter out = response.getWriter();
             try {
                 URL url = new URL("http://" + configCenter.getUcoolOnlineIp() + request.getRequestURI());
-                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-                fileEditor.pushStream(out, in, null, true);
+                urlReader.pushStream(response.getOutputStream(), url.openStream(), null, true);
             } catch (Exception e) {
             }
-            out.flush();
         }
-
     }
 }
